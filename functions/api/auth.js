fb6c2be20d09a08580c5c9fa05b3241a3bf0d079
ascal-dev <ascalrack@gmail.com>
2026-02-08 19:02:34 +0530

    Delete functions/api directory

--- functions/api/auth.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { request, env } = context;
-  const url = new URL(request.url);
-  const pw = request.headers.get("X-App-Password") || url.searchParams.get("pw");
-  
-  return Response.json({ ok: pw === env.APP_PASSWORD }, {
-    headers: { "Access-Control-Allow-Origin": "*" }
-  });
-}
